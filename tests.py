8fd3b8012d29adfc942fe990afe6ae2039d68d30
fei0324 <fl7834a@student.american.edu>
2018-08-19 16:36:55 -0400

    added tests for the search_triangles function

--- tests.py
+++ tests.py
@@ -2,7 +2,12 @@ import unittest
 import numpy as np
 from stl import mesh
 
-from subdivision import subdivision, subdivide_reconstruct
+from subdivision import (
+	subdivision,
+	subdivide_reconstruct,
+	calculate_centroid_of_triangle,
+	search_triangles,
+	)
 
 class TestSubdivision(unittest.TestCase):
 
@@ -52,22 +57,56 @@ class TestSubdivisionReconstruct(unittest.TestCase):
 		self.assertTrue(np.array_equal(self.oriTriangoSet[14], self.newTriangleSet[26]),
 						msg="Subdivided triangles are not inserted in the correct positions in the new triangle set.")
 
-		
+
 class TestSearchTriangles(unittest.TestCase):
 
 	def setUp(self):
 
 		self.test_dict = {2:'a', 1:'a', 3:'d', 5:'f', 4:'e', 6:'b', 7:'c', 8:'e'}
-		self.sorted_dict = {1:'a', 2:'a', 6:'b', 7:'c', 3:'d', 4:'e', 8:'e', 5:'f'}
+		self.sorted_keys = [1, 2, 6, 7, 3, 4, 8, 5]
+		self.triangle = np.array([np.array([1.,2.,3.]), np.array([-3.,9.,5.]), np.array([5.,2.,-3.])])
+
+		self.chime_mesh = mesh.Mesh.from_file("newChimeR.0127D4.stl")
+		self.ori_triangle_set = self.chime_mesh.vectors
+		self.impact_coor = np.array([0.,0.,0.])
+		self.output_indices = [25, 35, 44, 54, 23, 33]
 
 	def test_sort_dictionary_based_on_values_algorithm(self):
 
 		test_dict = self.test_dict
 		sorted_keys = sorted(test_dict, key=test_dict.__getitem__)
-		sorted_values = sorted(test_dict.values())
-		sorted_dict = dict(zip(sorted_keys, sorted_values))
 
-		self.assertEqual(sorted_dict, self.sorted_dict)
+		self.assertEqual(sorted_keys, self.sorted_keys)
+		self.assertEqual(test_dict[sorted_keys[0]], min(sorted(test_dict.values())))
+
+	def test_centroid_of_triangle(self):
+
+		# TEst the centroid is in the same plane as the triangle
+
+		pt1 = self.triangle[0]
+		pt2 = self.triangle[1]
+		pt3 = self.triangle[2]
+
+		v1 = pt2 - pt1
+		v2 = pt3 - pt1
+
+		centroid = calculate_centroid_of_triangle(pt1, pt2, pt3)
+
+		v3 = centroid - pt1
+
+		# find the normal vector of the plane
+		normalVec = np.cross(v1, v2)
+
+		# take the dot product of the normal vector and v3, expecting 0
+		dot_product = np.dot(v3, normalVec)
+
+		self.assertTrue(abs(dot_product)<10e-5, msg="The centroid is not on the same plane as the triangle.")
+
+	def test_correct_ordered_output(self):
+
+		indices = search_triangles(self.ori_triangle_set, self.impact_coor)
+		self.assertEqual(len(indices), 6, msg="The number of triangles in the output of the search function is incorrect.")
+		self.assertTrue(np.array_equal(indices, self.output_indices), msg="The search function is not outputing the correct triangles.")
 
 
 
